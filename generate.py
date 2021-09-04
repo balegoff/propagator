ae7c48951181a0be8fb834db4c3612babbc3f4b8
meteaure studios <legoff.baptiste@gmail.com>
2021-09-04 19:41:04 +0200

    run cmake from python script

--- generate.py
+++ generate.py
@@ -1,10 +1,11 @@
-import os, sys
+import os, sys, subprocess, platform
 
 repo_name = 'propagator'
 
 wc_dir = os.getcwd()
 
-# Check launch folder
+# Check launch folder -----
+
 if not os.path.isfile(repo_name + '/generate.py'):	
 	sys.exit('[Error] This script must be launched from parent folder (workspace)')
 
@@ -16,7 +17,27 @@ repos = {
 	"glfw": "https://github.com/glfw/glfw.git"
 }
 
-# Clone dependencies
+# Clone dependencies -----
+
 for dep in dependencies:
     if not os.path.exists(dep):
     	os.system('git clone ' + repos[dep])
+
+
+# Run cmake -----
+
+generated_dir = os.path.join(repo_name, "generated")
+
+if not os.path.exists(generated_dir):
+		os.makedirs(generated_dir)
+
+os.chdir(generated_dir)
+
+cmake_command = ['cmake', '..']
+
+if platform.system() == "Darwin":
+		cmake_command += ['-G', 'Xcode']
+
+subprocess.check_call(cmake_command)
+
+os.chdir(wc_dir)
